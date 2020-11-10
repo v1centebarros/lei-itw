591bc774e866c87ed1758854b379e9966d4c2bc1
Vicente Barros <vmabarros@ua.pt>
2020-11-10 14:27:56 +0000

    Update no exercício 02

--- aula05/ex02.js
+++ aula05/ex02.js
@@ -5,31 +5,34 @@ var resultado = document.getElementById("resultado");
 
 //Verifica se o utilizador inseriu algum valor nos inputs
 function verifica() {
-    if ((x.value !== "" && y.value !== "" && op.value!=="fac") || (x.value !== "" && op.value === "fac")){
+    if ((x.value !== "" && y.value !== "" && op.value !== "fac") || (x.value !== "" && op.value === "fac")) {
         calcular();
-    } 
+    }
 }
 
 
 function calcular() {
+    let x_val = parseFloat(x.value)
+    let y_val = parseFloat(y.value)
+
     switch (op.value) {
         case "soma":
-            resultado.value = parseFloat(x.value) + parseFloat(y.value);
+            resultado.value = x_val + y_val;
             break;
         case "sub":
-            resultado.value = parseFloat(x.value) - parseFloat(y.value);
+            resultado.value = x_val - y_val;
             break;
         case "mult":
-            resultado.value = parseFloat(x.value) * parseFloat(y.value);
+            resultado.value = x_val * y_val;
             break;
         case "div":
-            resultado.value = parseFloat(x.value) / parseFloat(y.value);
+            resultado.value = x_val / y_val;
             break;
         case "res":
-            resultado.value = parseFloat(x.value) % parseFloat(y.value);
+            resultado.value = x_val % y_val;
             break;
         case "fac":
-            resultado.value = fact(parseFloat(x.value));
+            resultado.value = fact(x_val);
             break;
         default:
             resultado.value = ""
@@ -41,7 +44,7 @@ function calcular() {
 function fact(num) {
     var res = 1;
     for (var i = 1; i <= num; i++) {
-        res *= i;         
+        res *= i;
     }
     return res;
 }
