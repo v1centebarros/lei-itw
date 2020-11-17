f083bc7d4be8a8a9326679cfbdbd139a122c2f31
Vicente Barros <vmabarros@ua.pt>
2020-11-17 15:33:59 +0000

    Aula 06 Terminada 🍭

---
+++ aula06/formulario.js
@@ -0,0 +1,83 @@
+﻿/* Função de validação  */
+function validate() {
+    var retVal = true; /* Vamos partir do princípio de que o formulário está válido ... */
+    retVal1 = validateNome()
+    retVal2 = validateMorada()
+    retVal3 = validateCurso();
+    retVal4 = validateVeiculos();
+    retVal5 = validateCor();
+
+    return retVal1 && retVal2 && retVal3 && retVal4 && retVal5;
+}
+
+function validateNome() {
+    nome = document.getElementById("Nome").value;
+    if (nome.length < 3) {
+        document.getElementById("NomeError").classList.remove("d-none");
+        return false;
+    } else {
+        if (!document.getElementById("NomeError").classList.contains("d-none")) {
+            document.getElementById("NomeError").classList.add("d-none");
+        }
+        return true
+    }
+}
+
+function validateMorada() {
+    morada = document.getElementById("Morada").value.trim().split(" ");
+    if (morada.length < 3) {
+        document.getElementById("MoradaError").classList.remove("d-none");
+        return false;
+    } else {
+        if (!document.getElementById("MoradaError").classList.contains("d-none")) {
+            document.getElementById("MoradaError").classList.add("d-none");
+        }
+        return true
+    }
+}
+
+function validateCurso() {
+    curso = document.getElementById("Curso").value;
+    if (curso === "") {
+        document.getElementById("CursoError").classList.remove("d-none");
+        return false;
+    } else {
+        if (!document.getElementById("CursoError").classList.contains("d-none")) {
+            document.getElementById("CursoError").classList.add("d-none");
+        }
+        return true
+    }
+}
+
+function validateVeiculos() {
+    veiculos = document.getElementsByName("vehicle");
+    count = 0;
+    veiculos.forEach(element => {
+        if (element.type == "checkbox" && element.checked) {
+            count++;
+        }
+    });
+
+    if (count < 2) {
+        document.getElementById("VehicleError").classList.remove("d-none");
+        return false;
+    } else {
+        if (!document.getElementById("VehicleError").classList.contains("d-none")) {
+            document.getElementById("VehicleError").classList.add("d-none");
+        }
+        return true
+    }
+}
+
+function validateCor() {
+    cores = document.querySelectorAll('input[name="cor"]:checked').length;
+    if (cores == 0) {
+        document.getElementById("CorError").classList.remove("d-none");
+        return false;
+    } else {
+        if (!document.getElementById("CorError").classList.contains("d-none")) {
+            document.getElementById("CorError").classList.add("d-none");
+        }
+        return true
+    }
+}
---
+++ aula06/totobola/totobola.js
@@ -0,0 +1,55 @@
+﻿//Código da Aula Teórica
+
+//--- variáveis globais
+var Status1 = document.getElementById("Status1");
+var StatusX = document.getElementById("StatusX");
+var Status2 = document.getElementById("Status2");
+var botao = document.getElementById("botao");
+var linhas = 0;
+var apostas = 1;
+var selected1 = 0;
+var selectedX = 0;
+var selected2 = 0;
+
+//--- função ativada sempre que se carrega em qualquer das caixas
+function boxClicked() {
+    linhas = 0;
+    apostas = 1;
+    selected1 = 0;
+    selectedX = 0;
+    selected2 = 0;
+    for (var i = 1; i <= 13; i++) {
+        var apostasNaLinha = 1;
+        var x = document.getElementById("Jogo" + i + "_1").checked;
+        var y = document.getElementById("Jogo" + i + "_x").checked;
+        var z = document.getElementById("Jogo" + i + "_2").checked;
+        if (x) selected1++;
+        if (y) selectedX++;
+        if (z) selected2++;
+        if (x | y | z) linhas++;
+        if ((x && y) || (x && z) || (y && z)) apostasNaLinha = 2;
+        if (x && y && z) apostasNaLinha = 3;
+        apostas *= apostasNaLinha;
+    }
+    console.log(apostas);
+    setStatus();
+}
+
+
+function setStatus() {
+    Status1.innerText = selected1;
+    StatusX.innerText = selectedX;
+    Status2.innerText = selected2;
+    if (linhas == 13 && apostas > 1 && apostas <= 384) {
+        var x = document.getElementById("multiplas" + apostas)
+        if (x != null)
+            x.checked = true;
+        botao.disabled = "";
+    } else {
+        var arr = document.getElementsByName("multiplas");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].checked = false;
+        }
+        botao.disabled = "disabled";
+    }
+}
